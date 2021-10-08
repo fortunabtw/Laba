4293283628a9d0a461064a489efb341594c3dbd3
fortunabtw <88847704+fortunabtw@users.noreply.github.com>
2021-10-08 14:26:59 +0300

    Update Laba1.java

--- Laba1.java
+++ Laba1.java
@@ -1,12 +1,14 @@
 import java.util.Random;
 import java.util.Set;
+
 import static java.lang.Math.*;
+
 import java.util.Scanner;
 
 public class Laba1 {
 
     public static void main(String[] args) {
-        Set<Integer> p = Set.of(2,3,5,6,8,10,13,16);
+        Set<Integer> lotsToCheck = Set.of(2, 3, 5, 6, 8, 10, 13, 16);
         int startA = 2;
         int endA = 17;
         int lenX = 19;
@@ -14,16 +16,16 @@ public class Laba1 {
         float endX = 3;
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите минимально возможный пропуск между элементами массива при выводе:");
-        int WhiteSpace = scan.nextInt();
+        int whiteSpace = scan.nextInt();
         scan.close();
-        if (WhiteSpace < 0){
-            WhiteSpace = 0;
+        if (whiteSpace < 0) {
+            whiteSpace = 0;
         }
 
         int[] a = creatArrayA(startA, endA); // пункт 1.
         float[] x = creatArrayX(startX, endX, lenX); //пункт 2.
-        double[][] Array = endArray(a, x, p); //пункт 3.
-        printMatrix(Array,maxLenValueEndArray(Array,WhiteSpace)); //пункт 4.
+        double[][] array = endArray(a, x, lotsToCheck); //пункт 3.
+        printMatrix(array, maxLenValueEndArray(array, whiteSpace)); //пункт 4.
     }
 
     public static int[] creatArrayA(int startA, int endA) {
@@ -37,63 +39,63 @@ public class Laba1 {
         return array;
     }
 
-    public static float[] creatArrayX(float startX,float endX,int lenX){
+    public static float[] creatArrayX(float startX, float endX, int lenX) {
         //метод создает массив х
         float[] array = new float[lenX];
-        for (int i = 0; i < lenX ; i++) {
-            array[i] = random(startX,endX);
+        for (int i = 0; i < lenX; i++) {
+            array[i] = random(startX, endX);
         }
         return array;
     }
 
-    public static float random(float startX,float endX) {
+    public static float random(float startX, float endX) {
         //метод для генерации рандомного числа
         Random r = new Random();
-        return (r.nextFloat()*(endX-startX) + startX);
+        return (r.nextFloat() * (endX - startX) + startX);
     }
 
-    public static double[][] endArray(int[] a, float[] x, Set<Integer> p) {
+    public static double[][] endArray(int[] a, float[] x, Set<Integer> lotsToCheck) {
         // метод создания двумерного массива
-        double[][] ae = new double[a.length][x.length];
+        double[][] endArray = new double[a.length][x.length];
 
-        for (int i = 0; i < ae.length; i++) {
-            for (int j = 0; j < ae[i].length; j++) {
+        for (int i = 0; i < endArray.length; i++) {
+            for (int j = 0; j < endArray[i].length; j++) {
                 if (a[i] == 17) {
-                    ae[i][j] = exp(asin(cos(x[j])));
-                } else if (p.contains(a[i])) {
-                    ae[i][j] = cos(pow((PI) * sin(x[j]), 2));
+                    endArray[i][j] = exp(asin(cos(x[j])));
+                } else if (lotsToCheck.contains(a[i])) {
+                    endArray[i][j] = cos(pow((PI) * sin(x[j]), 2));
                 } else {
-                    ae[i][j] = ((pow((pow(exp(x[j]), asin((x[j] - 1.5) / 9) - 1)) / 4, 2) - 1) / 1) / 3;
+                    endArray[i][j] = ((pow((pow(exp(x[j]), asin((x[j] - 1.5) / 9) - 1)) / 4, 2) - 1) / 1) / 3;
                 }
             }
         }
-        return ae;
+        return endArray;
     }
 
-    public static void printMatrix(double[][] Array,int MaxLenIntValue){
+    public static void printMatrix(double[][] array, int maxLenIntValue) {
         //метод для вывода конечного массива
-        String MaxLen = Integer.toString(MaxLenIntValue);
+        String maxLen = Integer.toString(maxLenIntValue);
 
-        for (double[] doubles : Array) {
+        for (double[] doubles : array) {
             for (double aDouble : doubles) {
-                System.out.printf("%"+MaxLen+".4f", aDouble);
+                System.out.printf("%" + maxLen + ".4f", aDouble);
             }
             System.out.println();
         }
     }
 
-    public static int maxLenValueEndArray(double[][] Array,int o) {
+    public static int maxLenValueEndArray(double[][] array, int whiteSpace) {
         //поиск максимальной длины элемента в массиве
         int max = 0;
-        for (double[] doubles : Array){
-            for (double doubles1 : doubles){
+        for (double[] doubles : array) {
+            for (double doubles1 : doubles) {
                 int len = Integer.toString((int) doubles1).length();
-                if (len > max){
+                if (len > max) {
                     max = len;
                 }
             }
         }
-        return max + 5 + o;
+        return max + 5 + whiteSpace;
         //+4 т.к. еще 4 знака после запятой
         //+1 для учета ","
     }
